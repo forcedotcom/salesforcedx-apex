8e64745678167c74e62e85f959da534369003e27
Smit Shah <smit.shah@salesforce.com>
2020-06-15 16:41:19 -0500

    handle logId and numberOfLogs

--- packages/apex/src/commands/apexLogGet.ts
+++ packages/apex/src/commands/apexLogGet.ts
@@ -8,37 +8,9 @@ export class ApexLogGet {
     this.connection = connection;
   }
 
-  public async execute(logID: string) {
-    //invalid  --> check for invalid flag and throw error
-    //if no parameter than return the latest log
-    //u --> default org if flag is not used
-    //apiversion --> ??
-    //number  --> call getMultipleLogs()
-    //logid  --> call getSingleLog()
-    //json  --> if json flag is true then no color return plain
-    //color  --> no json flag, check for color flag, color the log lines
-    //loglevel --> store the logs at $HOME/.sfdx/sfdx.log
-  }
-
-  // get the logs for the specified logid
-  //if not mentioned then return the latest log
-  public async getSingleLog(logID: string) {
-    const url = util.format(
-      '%s/services/data/v%s/tooling/sobjects/ApexLog/%s/Body',
-      this.connection.instanceUrl,
-      this.connection.version,
-      logID
-    );
-    const response = await this.connection.request(url);
-    return response;
-  }
-
-  //return the n most recent logs
-  public async getMultipleLogs(numberOfLogs: number) {
-    const restrictLogs =
-      numberOfLogs && numberOfLogs > 0
-        ? `+DESconst C+LIMIT+${numberOfLogs}`
-        : '';
+  // return the n most recent log id's
+  public async getLogIds(numberOfLogs: number) {
+    const restrictLogs = numberOfLogs > 0 ? `+DESC+LIMIT+${numberOfLogs}` : '';
     const url = util.format(
       '%s/services/data/v%s/tooling/query/?q=' +
         'Select+Id,+Application,+DurationMilliseconds,+Location,+LogLength,+LogUser.Name,+Operation,+Request,StartTime,+Status+' +
@@ -49,25 +21,36 @@ export class ApexLogGet {
       this.connection.version,
       restrictLogs
     );
-
-    // List of id's
     const response = await this.connection.request(url);
-    return response;
+    const records = JSON.stringify(response);
 
-    // print response to check its data
+    // To store log id's in an array
+    let logId: string[] = [];
+    JSON.parse(records, (key, value) => {
+      if (key === 'Id') {
+        logId.push(value);
+      }
+    });
+    return logId;
   }
 
+  /**
+   * Handle -n and -i flags for force:apex:log:get
+   * Output log bodies of last N number of logs
+   * @param numberOfLogs {number} number of logs to retrieve
+   * @param logId {string} logId - the debug log to retrieve
+   */
   public async getLogs(numberOfLogs?: number, logId?: string) {
     let logIdList: string[] = [];
     if (numberOfLogs) {
-      //  const result = helper(numberOfLogs);
-      // get log ids for each log -> make a list of log ids
+      logIdList = await this.getLogIds(numberOfLogs);
     } else {
       logIdList.push(logId);
-      // add logId to list of ids
     }
 
     let logRecords: string[] = [];
+
+    // Given logId retrieve log
     for (let i = 0; i < logIdList.length; i++) {
       const url = util.format(
         '%s/services/data/v%s/tooling/sobjects/ApexLog/%s/Body',
@@ -80,8 +63,5 @@ export class ApexLogGet {
       logRecords.push(stringResponse);
     }
     return logRecords;
-    // build url
-    //for every id in list of log ids
-    // connection.request(url)
   }
 }
