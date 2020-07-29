a5f9794ce1f66e56b78895b4be25353ad7d31c5e
Ananya <a.jha@salesforce.com>
2020-07-29 11:13:40 -0500

    added color and formatting

--- packages/plugin-apex/src/commands/apex/execute.ts
+++ packages/plugin-apex/src/commands/apex/execute.ts
@@ -4,20 +4,29 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { ApexExecuteOptions, ExecuteService } from '@salesforce/apex-node';
+import {
+  ApexExecuteOptions,
+  ExecuteAnonymousResponse,
+  ExecuteService
+} from '@salesforce/apex-node';
 import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+import * as chalk from 'chalk';
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/plugin-apex', 'execute');
 
+const success = chalk.bold.green;
+const error = chalk.bold.red;
+
 export default class Execute extends SfdxCommand {
   public static description = messages.getMessage('commandDescription');
+  public static longDescription = messages.getMessage('longDescription');
 
   public static examples = [
-    `Executes one or more lines of Apex code entered on the command line, or executes the code in a local file. To execute your code interactively, run this command with no parameters. At the prompt, enter all your Apex code; press CTRL-D when you're finished. Your code is then executed in a single execute anonymous request. For more information, see "Anonymous Blocks" in the Apex Developer Guide.\n\nExamples:\n`,
-    '   $ sfdx force:apex:execute -f ~/test.apex',
-    '   $ sfdx force:apex:execute \n   >> Start typing Apex code. Press the Enter key after each line,\n   >> then press CTRL+D when finished.'
+    `$ sfdx force:apex:execute -f ~/test.apex`,
+    `$ sfdx force:apex:execute \nStart typing Apex code. Press the Enter key after each line, then press CTRL+D when finished.`
   ];
   protected static supportsUsername = true;
 
@@ -48,23 +57,50 @@ export default class Execute extends SfdxCommand {
     apiversion: flags.builtin()
   };
 
-  public async run(): Promise<void> {
+  public async run(): Promise<AnyJson> {
     if (!this.org) {
       throw new Error(
         'Must pass a username and/or OAuth options when creating an AuthInfo instance.'
       );
     }
-    const conn = this.org!.getConnection();
+    const conn = this.org?.getConnection();
+    // @ts-ignore
     const exec = new ExecuteService(conn);
 
     const execAnonOptions: ApexExecuteOptions = {
       ...(this.flags.apexcodefile
         ? { apexFilePath: this.flags.apexcodefile }
-        : {})
+        : { userInput: true })
     };
     const result = await exec.executeAnonymous(execAnonOptions);
     if (this.flags.json) {
-      this.ux.log(result.result.logs);
+      return result.result;
     }
+    this.ux.log(this.formatResult(result));
+    return this.formatResult(result);
+  }
+
+  private formatResult(response: ExecuteAnonymousResponse): string {
+    let outputText = '';
+    if (response.result.compiled === true) {
+      outputText += `${success(
+        messages.getMessage('execute_compile_success')
+      )}\n`;
+      if (response.result.success === true) {
+        outputText += `${success(
+          messages.getMessage('execute_runtime_success')
+        )}\n`;
+      } else {
+        outputText += error(`Error: ${response.result.exceptionMessage}\n`);
+        outputText += error(`Error: ${response.result.exceptionStackTrace}\n`);
+      }
+      outputText += `\n${response.result.logs}`;
+    } else {
+      outputText += error(
+        `Error: Line: ${response.result.line}, Column: ${response.result.column}\n`
+      );
+      outputText += error(`Error: ${response.result.compileProblem}\n`);
+    }
+    return outputText;
   }
 }
