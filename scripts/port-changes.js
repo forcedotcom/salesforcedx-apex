86b95ff43c5f0a2e397e0349de720102323331bb
Shelby <shelby.holden@salesforce.com>
2020-10-01 13:05:22 -0600

    chore: fix typo

--- scripts/port-changes.js
+++ scripts/port-changes.js
@@ -100,7 +100,7 @@ function isTrueDiff(commitMap) {
     var developResult = shell.exec(`git log --grep="${commitMap[MESSAGE]}" --oneline develop`, { silent: true });
     if (!mainResult || mainResult.length === 0) {
         if (ADD_VERBOSE_LOGGING)
-            console.log(`Commit is missing from main. Porting.\n\t${commitMap[COMMIT]} ${commitMap[MESSAGE]}\n\rMain: ${mainResult}\n\tDevelop: ${developResult}`);
+            console.log(`Commit is missing from main. Porting.\n\t${commitMap[COMMIT]} ${commitMap[MESSAGE]}\n\tMain: ${mainResult}\n\tDevelop: ${developResult}`);
         return true;
     } else if (mainResult && developResult) {
         // if (ADD_VERBOSE_LOGGING)
