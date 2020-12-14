06b34d6279bae68a259fe3f15db885e8d2dc4b00
AnanyaJha <a.jha@salesforce.com>
2020-12-14 14:52:06 -0600

    fix: remove extra copyright header
    Co-authored-by: Bryan Powell <b.powell@salesforce.com>

--- packages/apex-node/test/reporters/humanReporter.test.ts
+++ packages/apex-node/test/reporters/humanReporter.test.ts
@@ -8,12 +8,6 @@ import { expect } from 'chai';
 import { HumanReporter } from '../../src';
 import { successResult, testResults, coverageResult } from './testResults';
 
-/*
- * Copyright (c) 2020, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
 describe('Human Reporter Tests', () => {
   const reporter = new HumanReporter();
 
