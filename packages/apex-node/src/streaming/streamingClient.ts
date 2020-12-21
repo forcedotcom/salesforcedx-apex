d7bc37c8187b8dc45145de44cea1fa18c4519ecb
AnanyaJha <a.jha@salesforce.com>
2020-12-21 10:33:46 -0600

    Update packages/apex-node/src/streaming/streamingClient.ts

--- packages/apex-node/src/streaming/streamingClient.ts
+++ packages/apex-node/src/streaming/streamingClient.ts
@@ -74,7 +74,7 @@ export class StreamingClient {
     });
   }
 
-  // There's an intermittent auth issue with Streaming API that requires the connection to be reset
+  // NOTE: There's an intermittent auth issue with Streaming API that requires the connection to be refreshed
   // The builtin org.refreshAuth() util only refreshes the connection associated with the instance of the org you provide, not all connections associated with that username's orgs
   public async init(): Promise<void> {
     await refreshAuth(this.conn);
