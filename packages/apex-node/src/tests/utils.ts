d8d32eb9aeace8c7a85b51c84d2f03ed7c2f649e
AnanyaJha <a.jha@salesforce.com>
2021-03-25 15:45:30 -0700

    Support running tests from a subscriber org/managed package (#170)
    @W-9010476@

--- packages/apex-node/src/tests/utils.ts
+++ packages/apex-node/src/tests/utils.ts
@@ -5,7 +5,9 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { Connection } from '@salesforce/core';
 import { CLASS_ID_PREFIX, TEST_RUN_ID_PREFIX } from './constants';
+import { NamespaceInfo, NamespaceQueryResult } from './types';
 
 export function isValidTestRunID(testRunId: string): boolean {
   return (
@@ -19,4 +21,27 @@ export function isValidApexClassID(apexClassId: string): boolean {
     (apexClassId.length === 15 || apexClassId.length === 18) &&
     apexClassId.startsWith(CLASS_ID_PREFIX)
   );
+}
+
+export async function queryNamespaces(
+  connection: Connection
+): Promise<NamespaceInfo[]> {
+  const installedNsQuery = 'SELECT NamespacePrefix FROM PackageLicense';
+  const installedNsPromise = connection.query(installedNsQuery) as Promise<
+    NamespaceQueryResult
+  >;
+  const orgNsQuery = 'SELECT NamespacePrefix FROM Organization';
+  const orgNsPromise = connection.query(orgNsQuery) as Promise<
+    NamespaceQueryResult
+  >;
+
+  const allNamespaces = await Promise.all([installedNsPromise, orgNsPromise]);
+  const installedNamespaces = allNamespaces[0].records.map(record => {
+    return { installedNs: true, namespace: record.NamespacePrefix };
+  });
+  const orgNamespaces = allNamespaces[1].records.map(record => {
+    return { installedNs: false, namespace: record.NamespacePrefix };
+  });
+
+  return [...orgNamespaces, ...installedNamespaces];
 }
