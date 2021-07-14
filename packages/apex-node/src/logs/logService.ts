c80fcb09b51c95241015d8b3b8729a4b422b8a5e
Randi Wilson <randi274@gmail.com>
2021-07-14 18:37:18 -0500

    feat: adds apex tail log helper functions

--- packages/apex-node/src/logs/logService.ts
+++ packages/apex-node/src/logs/logService.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { Connection } from '@salesforce/core';
+import { Connection, SfdxError } from '@salesforce/core';
 import {
   ApexLogGetOptions,
   LogQueryResult,
@@ -14,10 +14,14 @@ import {
 import { createFile } from '../utils';
 import { nls } from '../i18n';
 import * as path from 'path';
+import * as dayjs from 'dayjs';
 import { AnyJson } from '@salesforce/ts-types';
+import { QueryResult } from '../utils/types';
 
+const DEFAULT_DEBUG_LEVEL_NAME = 'SFDC_DevConsole';
+const LOG_TYPE = 'DEVELOPER_LOG';
 const MAX_NUM_LOGS = 25;
-
+const TAIL_LISTEN_TIMEOUT_MIN = 30;
 export class LogService {
   public readonly connection: Connection;
 
@@ -49,6 +53,7 @@ export class LogService {
     const connectionRequests = logIdList.map(async id => {
       const url = `${this.connection.tooling._baseUrl()}/sobjects/ApexLog/${id}/Body`;
       const logRecord = await this.toolingRequest(url);
+      //const logRecord = await this.getLogById(id);
       if (options.outputDir) {
         const logPath = path.join(options.outputDir, `${id}.log`);
         logPaths.push(logPath);
@@ -71,6 +76,12 @@ export class LogService {
     });
   }
 
+  public async getLogById(logId: string): Promise<AnyJson> {
+    const url = `${this.connection.tooling._baseUrl()}/sobjects/ApexLog/${logId}/Body`;
+    const response = (await this.connection.tooling.request(url)) as AnyJson;
+    return { log: response.toString() || '' };
+  }
+
   public async getLogRecords(numberOfLogs?: number): Promise<LogRecord[]> {
     let query = 'Select Id, Application, DurationMilliseconds, Location, ';
     query +=
@@ -90,8 +101,65 @@ export class LogService {
     return response.records as LogRecord[];
   }
 
+  private async createTraceFlag(userId: string): Promise<void> {
+    const DebugLevelId = await this.getDebugLevelId(DEFAULT_DEBUG_LEVEL_NAME);
+    const traceFlagDate = dayjs();
+    const traceFlag = {
+      LogType: LOG_TYPE,
+      TracedEntityId: userId,
+      StartDate: traceFlagDate.format(),
+      ExpirationDate: traceFlagDate
+        .clone()
+        .add(TAIL_LISTEN_TIMEOUT_MIN, 'minutes')
+        .format(),
+      DebugLevelId
+    };
+
+    await this.connection.tooling.create('TraceFlag', traceFlag);
+  }
+
+  private async getDebugLevelId(debugLevel: string): Promise<string> {
+    const debugLevelResult = await this.connection.tooling.query<{
+      Id: string;
+    }>(`SELECT Id FROM DebugLevel WHERE DeveloperName = '${debugLevel}'`);
+    if (!this.hasRecords(debugLevelResult)) {
+      throw new SfdxError(`Debug Level not found for '${debugLevel}'`);
+    }
+
+    return debugLevelResult.records[0].Id;
+  }
+
+  private async usernameToUserId(username: string): Promise<string> {
+    return (
+      await this.connection.singleRecordQuery<{ Id: string }>(
+        `SELECT Id FROM User WHERE Username = '${username}'`
+      )
+    ).Id;
+  }
+
+  private async getTraceFlag(
+    userId: string
+  ): Promise<
+    QueryResult<{
+      ExpirationDate: string;
+      StartDate: string;
+      DebugLevelId: string;
+    }>
+  > {
+    let traceQuery =
+      'SELECT Id, DebugLevelId, StartDate, ExpirationDate FROM TraceFlag ';
+    traceQuery += `WHERE TracedEntityId = '${userId}' AND LogType = '${LOG_TYPE}'`;
+    traceQuery += `ORDER BY CreatedDate DESC LIMIT 1'`;
+    return await this.connection.tooling.query(traceQuery);
+  }
+
   public async toolingRequest(url: string): Promise<AnyJson> {
     const log = (await this.connection.tooling.request(url)) as AnyJson;
     return log;
+  }
+
+  // eslint-disable-next-line
+  private hasRecords(result: any): boolean {
+    return result?.records?.length > 0;
   }
 }
