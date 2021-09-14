44977112906db0e34ab3236ba87b89e62cb55924
Jeff Beeghly <jbeeghly@salesforce.com>
2021-09-14 14:47:01 -0700

    fix: 414-from-long-url

--- packages/apex-node/src/tests/constants.ts
+++ packages/apex-node/src/tests/constants.ts
@@ -6,7 +6,8 @@
  */
 
 // Tooling API query char limit is 100,000 after v48; REST API limit for uri + headers is 16,348 bytes
-// local testing shows query char limit to be closer to ~12,300
-export const QUERY_CHAR_LIMIT = 12300;
+// local testing shows query char limit to be closer to ~12,300.
+// Through experimentation, the record limit is around 640 before the REST API limit is hit.
+export const QUERY_RECORD_LIMIT = 640;
 export const CLASS_ID_PREFIX = '01p';
 export const TEST_RUN_ID_PREFIX = '707';
