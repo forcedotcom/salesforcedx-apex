20d808e443b034cfc6536e27fe2fc3c0205c7be7
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2022-01-04 14:46:31 -0800

    test: check start time format with custom locale

--- packages/apex-node/test/reporters/junitReporter.test.ts
+++ packages/apex-node/test/reporters/junitReporter.test.ts
@@ -6,6 +6,7 @@
  */
 import { expect } from 'chai';
 import { JUnitReporter } from '../../src';
+import { formatStartTime, getCurrentTime } from '../../src/utils';
 import {
   testResults,
   junitResult,
@@ -61,5 +62,18 @@ describe('JUnit Reporter Tests', () => {
     expect(result).to.not.be.empty;
     expect(result).to.eql(junitCodeCov);
     expect(result).to.contain('orgWideCoverage');
+  });
+
+  it('should format start time even when custom locale is used', () => {
+    // set process.env LC_ALL=en_CA
+    process.env.LC_ALL = 'en_CA';
+    const date = new Date();
+    const testStartTime = `${date.toDateString()} ${date.toLocaleTimeString()}`;
+    // TODO: add regex check for presence of a.m / pm. in testStartTime
+    // expect(new RegExp(/.*[a|p]\.m\.$/g).test(testStartTime)).to.equal(true);
+    const timestamp = new Date(testStartTime).toISOString();
+    // Date value should be the same after formatting with locale
+    // Ignore the loss of precision (last field of milliseconds) in formatting conversion
+    expect(timestamp.split('.')[0]).to.equal(date.toISOString().split('.')[0]);
   });
 });
